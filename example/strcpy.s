e4d3f1894ac76098bcfb2823540825b06f04e2bd
Nick Knight <nick.knight@sifive.com>
2020-04-29 22:08:32 -0700

    Fix register numbering to handle increased LMUL (#438)

--- example/strcpy.s
+++ example/strcpy.s
@@ -6,14 +6,14 @@ strcpy:
       mv a2, a0             # Copy dst
       li t0, -1             # Infinite AVL
 loop:
-    vsetvli x0, t0, e8,m8   # Max length vectors of bytes
-    vleff.v v1, (a1)        # Get src bytes
+    vsetvli x0, t0, e8, m8   # Max length vectors of bytes
+    vleff.v v8, (a1)        # Get src bytes
       csrr t1, vl           # Get number of bytes fetched
-    vmseq.vi v0, v1, 0      # Flag zero bytes
+    vmseq.vi v0, v8, 0      # Flag zero bytes
     vfirst.m a3, v0         # Zero found?
       add a1, a1, t1        # Bump pointer
     vmsif.m v0, v0          # Set mask up to and including zero byte.
-    vse.v v1, (a2), v0.t    # Write out bytes
+    vse.v v8, (a2), v0.t    # Write out bytes
       add a2, a2, t1        # Bump pointer
       bltz a3, loop         # Zero byte not found, so loop
 
