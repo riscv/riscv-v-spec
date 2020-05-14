4ff6c6b2207961f56dffe7736a07f1b8720285da
Krste Asanovic <krste@eecs.berkeley.edu>
2020-05-14 02:53:40 -0700

    Updated/clarified mask overwrite rules.

--- example/strcpy.s
+++ example/strcpy.s
@@ -9,10 +9,10 @@ loop:
     vsetvli x0, t0, e8, m8   # Max length vectors of bytes
     vle8ff.v v8, (a1)        # Get src bytes
       csrr t1, vl           # Get number of bytes fetched
-    vmseq.vi v0, v8, 0      # Flag zero bytes
-    vfirst.m a3, v0         # Zero found?
+    vmseq.vi v1, v8, 0      # Flag zero bytes
+    vfirst.m a3, v1         # Zero found?
       add a1, a1, t1        # Bump pointer
-    vmsif.m v0, v0          # Set mask up to and including zero byte.
+    vmsif.m v0, v1          # Set mask up to and including zero byte.
     vse8.v v8, (a2), v0.t    # Write out bytes
       add a2, a2, t1        # Bump pointer
       bltz a3, loop         # Zero byte not found, so loop
--- example/strncpy.s
+++ example/strncpy.s
@@ -7,11 +7,11 @@ strncpy:
 loop:
     vsetvli x0, a2, e8,m8   # Vectors of bytes.
     vle8ff.v v8, (a1)        # Get src bytes
-    vmseq.vi v0, v8, 0      # Flag zero bytes
-    vfirst.m a4, v0         # Zero found?
-    vmsif.m v0, v0          # Set mask up to and including zero byte.
-    vse8.v v8, (a3), v0.t    # Write out bytes
+    vmseq.vi v1, v8, 0      # Flag zero bytes
       csrr t1, vl           # Get number of bytes fetched
+    vfirst.m a4, v1         # Zero found?
+    vmsif.m v0, v1          # Set mask up to and including zero byte.
+    vse8.v v8, (a3), v0.t    # Write out bytes
       sub a2, a2, t1        # Decrement count.
       bgez a4, zero_tail    # Zero remaining bytes.
       add a1, a1, t1        # Bump pointer
