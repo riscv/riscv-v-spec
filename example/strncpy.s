0f7b1e668bdd7a0173efa9d3b84ff35ab45e2b41
Jerry Shih <bignose1007@gmail.com>
2020-08-26 13:51:44 +0800

    Update strcpy and strncpy example.
    1. skip the "vmsif" instruction.
    2. update the indent for "vsetvli" instruction.

--- example/strncpy.s
+++ example/strncpy.s
@@ -5,13 +5,13 @@
 strncpy:
       mv a3, a0             # Copy dst
 loop:
-    vsetvli x0, a2, e8,m8, ta,ma   # Vectors of bytes.
+    vsetvli x0, a2, e8, m8, ta, ma # Vectors of bytes.
     vle8ff.v v8, (a1)        # Get src bytes
     vmseq.vi v1, v8, 0      # Flag zero bytes
       csrr t1, vl           # Get number of bytes fetched
     vfirst.m a4, v1         # Zero found?
-    vmsif.m v0, v1          # Set mask up to and including zero byte.
-    vse8.v v8, (a3), v0.t    # Write out bytes
+    vsetvli x0, a4, e8, m8, ta, ma # Setup vl including zero byte.
+    vse8.v v8, (a3)          # Write out bytes
       sub a2, a2, t1        # Decrement count.
       bgez a4, zero_tail    # Zero remaining bytes.
       add a1, a1, t1        # Bump pointer
@@ -21,11 +21,11 @@ loop:
       ret
 
 zero_tail:
-    vsetvli x0, a2, e8,m8,ta,ma   # Vectors of bytes.
+    vsetvli x0, a2, e8, m8, ta, ma # Vectors of bytes.
     vmv.v.i v0, 0           # Splat zero.
 
 zero_loop:
-    vsetvli t1, a2, e8,m8,ta,ma   # Vectors of bytes.
+    vsetvli t1, a2, e8, m8, ta, ma # Vectors of bytes.
     vse8.v v0, (a3)          # Store zero.
       sub a2, a2, t1        # Decrement count.
       add a3, a3, t1        # Bump pointer
